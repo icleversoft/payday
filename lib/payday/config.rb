b596d6b8da1174bc0a368057eb7e44f866498139
George Stavrou <iphone@icleversoft.com>
2015-04-16 21:09:35 +0300

    added stamp assets according to invoice status

--- lib/payday/config.rb
+++ lib/payday/config.rb
@@ -3,7 +3,7 @@ module Payday
   # Payday::Config.default.company_name = "Awesome Corp".
   class Config
     attr_accessor :invoice_logo, :company_name, :company_details, :date_format, :currency
-
+    attr_reader :stamps
     # Sets the page size to use. See the
     # {http://prawn.majesticseacreature.com/docs/0.10.2/Prawn/Document/PageGeometry.html Prawn documentation} for valid
     # page_size values.
@@ -19,12 +19,16 @@ module Payday
     # Primarily intended for use in our tests.
     def reset
       # TODO: Move into specs and make minimal configuration required (company name / details)
-      self.invoice_logo = File.join(File.dirname(__FILE__), "..", "..", "spec", "assets", "default_logo.png")
+      assets_dir = File.join( File.dirname(__FILE__), "..", "..", "spec", "assets" )
+      self.invoice_logo = File.join(assets_dir, "default_logo.png")
       self.company_name = "Awesome Corp"
       self.company_details = "awesomecorp@commondream.net"
       self.date_format = "%B %e, %Y"
       self.currency = "USD"
       self.page_size = "LETTER"
+      self.stamps = {paid: File.join(assets_dir, "paid.png"),
+                     overdue: File.join(assets_dir, "past_due.png"),
+                     refunded: File.join(assets_dir, "refunded.png")}
     end
 
     # Internal: Contruct a new config object.
